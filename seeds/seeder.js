3630f76e603b9775eeaceb474ae805f850de4b28
harrdev <jasonharrphx@live.com>
2022-01-08 11:48:41 -0700

    database seeding working > > Co-authored-by: Hannah <hannahrlegros@gmail.com>

--- seeds/seeder.js
+++
@@ -1,9 +0,0 @@
-// const { Seeder } = require('mongo-seeding');
-
-// const config = {
-//     database: 'mongodb://127.0.0.1:27017/asanas',
-//     dropDatabase: true,
-//   };
-
-//   const seeder = new Seeder(config);
-
